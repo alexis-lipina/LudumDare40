a9c5013ae4fcf21133b21cb7275a0f9c87a5f30e
Dane Sherman (RIT Student) <dms7827@ad.rit.edu>
2017-12-02 20:24:40 -0500

    Cholesteral

---
+++ VirusLD40/Assets/Scripts/AttractCholesterol.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractCholesterol : MonoBehaviour
+{
+    [SerializeField] private float attractiveForce;
+    private Rigidbody2D rb;
+    private Vector3 attraction = Vector3.zero;
+
+    //for other cholesteral cells to check
+    public Vector3 Attraction { get {return attraction; } }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        switch (coll.gameObject.tag)
+        {
+            case "RightWall":
+                attraction = coll.transform.right;
+                break;
+            case "LeftWall":
+                attraction = coll.transform.right * -1;
+                
+                break;
+            case "Cholesterol":
+                AttractCholesterol script = coll.gameObject.GetComponent<AttractCholesterol>();
+                attraction = script.Attraction;
+                break;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(attraction != Vector3.zero)
+        {
+            rb.AddForce(attraction * attractiveForce);
+        }
+    }
+}
