b08f68bbd42c9073c4437990e9dee927c6c48858
Mark Lipina <mal4739@g.rit.edu>
2017-12-03 11:13:24 -0500

    Made the UI Manager, UI should be good to go-ish

---
+++ VirusLD40/Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] List<GameObject> keyslist;
+
+
+
+    void Start()
+    {
+        keyslist = new List<GameObject>();
+        
+    }
+
+    void UpdateUI(List<bool> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            keyslist[i].SetActive(list[i]);
+        }
+    }
+}
